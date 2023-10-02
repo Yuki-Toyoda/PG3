68b523f2eb29e3849826878a1dfdd970ffd95a03
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2023-10-02 12:17:01 +0900

    01_02_確認課題

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,34 +1,44 @@
 #include <stdio.h>
 
 /// <summary>
-/// 引数の値二つを比較し、小さい値を表示する関数
+/// 一般の賃金体制の計算関数
 /// </summary>
-/// <typeparam name="T">引数の型</typeparam>
-/// <param name="a">引数1(数字以外は不可)</param>
-/// <param name="b">引数1(数字以外は不可)</param>
-/// <returns>2つのうちの小さい値</returns>
-template <typename T>
-T Min(T a, T b) {
-	// 引数の比較をして小さい方を返す
-	return a < b ? a : b;
+/// <param name="WorkTime">働いた時間</param>
+/// <returns>賃金</returns>
+int NormalWage(int workTime) {
+	return (1072 * workTime);
 }
-// char型が代入されている場合は入力不可なことを示す
-template<>
-char Min<char>(char a, char b) {
-	// 数字以外は代入できない
-	printf("数字以外は代入出来ません");
-	return '\0';
+
+/// <summary>
+/// 再帰的な賃金体制の計算関数
+/// </summary>
+/// <param name="workTime">働いた時間</param>
+/// <param name="preWage">一時間前の給料</param>
+/// <returns></returns>
+int RecursionWage(int workTime, int preWage = 100) {
+	// 給料計算
+	int wage = preWage * 2 - 50;
+	if (workTime <= 0) {
+		return 0;
+	}
+
+	// 一時間当たりの賃金
+	printf("残り%d時間時点 ... %d\n", workTime - 1, preWage);
+
+	return preWage + RecursionWage(workTime - 1, wage);
 }
 
 // メイン関数
 int main() {
-	// int型
-	printf("%d\n", Min<int>(1, 5));
-	// float型
-	printf("%f\n", Min<float>(1.2f, 5.4f));
-	// double型
-	printf("%lf\n", Min<double>(1.8f, 9.4f));
-	// char型
-	printf("%c\n", Min<char>('1', '3'));
+	while (1) {
+		// 働いた時間変数
+		int workTime = 0;
+		// 働いた時間を入力する
+		printf("働いた時間を入力 ... ");
+		scanf_s("%d", &workTime);
+		// 入力した時間を元に賃金を計算し出力
+		printf("一般的な賃金体制の場合 ... %d\n\n", NormalWage(workTime));
+		printf("再帰的な賃金体制の場合 ... %d\n\n", RecursionWage(workTime));
+	}
 	return 0;
 }
