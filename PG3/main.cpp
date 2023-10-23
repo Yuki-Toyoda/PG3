acac2d107f29bae2e32de0819702e2117fd99383
ToyodaYuki <115054848+Yuki-Toyoda@users.noreply.github.com>
2023-10-23 12:06:08 +0900

    02_01_確認課題修了

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include "Enemy.h"
 
 // メイン関数
 int main() {
+
+	// 敵を生成
+	Enemy* enemy_ = new Enemy(); // インスタンス生成
+	enemy_->Initialize(); // 初期化
+
+	// 更新処理を呼び続ける
+	while (1)
+		enemy_->Update();
+
+	// 敵のインスタンス削除
+	delete enemy_;
 	return 0;
 }
